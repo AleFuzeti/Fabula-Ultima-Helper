e7c895a729a3cd47b1ddcb6a503c8caab518708c
fuzeti <alebertipaglia@gmail.com>
2025-04-17 19:47:00 -0300

    weapons images system (no images)

--- src/components/EquipmentList.js
+++ src/components/EquipmentList.js
@@ -33,31 +33,47 @@ const EquipmentList = ({ type, filters }) => {
   const renderEquipmentCard = (item) => {
     return (
       <div key={item.name} className="equipment-card">
-        <div className="equipment-book">
-          <span></span>
-          <span>{item.book}</span>
-        </div>
-        <div className="equipment-category">
-          <span>{item.rare}</span>
-          <span>{item.category}</span>
-        </div>
-        <div className="equipment-header">
-        <span>
-            {item.name}{" "}
-            {item.martial === "Yes" && (
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 408.98 412.19" width="15"height="15">
-                <g id="martialFilled" data-name="martialFilled"> <path className="martialFilled" d="M380.29,206.1c-69,33.4-137.3,100.5-175.8,178-38.5-77.5-106.8-144.6-175.8-178C97.69,172.7,165.99,105.6,204.49,28.1c38.5,77.5,106.8,144.6,175.8,178Z"></path> </g>
-              </svg>
-            )}
-          </span>
-          <span>{item.cost} z</span>
-        </div>
 
+      <div className="header">
+        <img
+              src={`${process.env.PUBLIC_URL}/assets/Weapons/loading.gif`}
+              alt={item.name}
+              className="equip-image"
+            />
+        <div>
+          <div className="equipment-book">
+            <span>
+            </span>
+            <span>{item.book}</span>
+          </div>
+
+          <div className="equipment-category">
+            <span>{item.rare}</span>
+            <span>{item.category}</span>
+          </div>
+
+          <div className="equipment-header">
+          <span>
+              {item.name}{" "}
+              {item.martial === "Yes" && (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 408.98 412.19" width="15"height="15">
+                  <g id="martia</g>lFilled" data-name="martialFilled"> <path className="martialFilled" d="M380.29,206.1c-69,33.4-137.3,100.5-175.8,178-38.5-77.5-106.8-144.6-175.8-178C97.69,172.7,165.99,105.6,204.49,28.1c38.5,77.5,106.8,144.6,175.8,178Z"></path> </g>
+                </svg>
+              )}
+            </span>
+            <span>{item.cost} z</span>
+          </div>
+
+        </div>
+      </div>
+       {(type === "weapon") && (
         <div className="equipment-stats">
           <span>{item.accuracy} {item.acc_mod}</span>
           <span>{item.damage} {item.damage_type}</span>
         </div>
+        )}
 
+        {((type === "armor") || (type === "shield")) && (
         <>
         <div className="equipment-stats">
           <span>Defense</span>
@@ -75,6 +91,7 @@ const EquipmentList = ({ type, filters }) => {
           )}
         </div>
         </>
+        )}
 
         <div className="equipment-details">
           <span>{item.hand}</span>
